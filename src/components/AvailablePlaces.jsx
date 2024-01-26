04d66b967ba41cd74f3f8426c0af7e721dd7a399
Roberto <roberto.quadraccia90@gmail.com>
2024-01-26 11:13:20 +0100

    Fallback text while fetching data implemented

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,22 +2,25 @@ import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
+    setIsFetching(true);
     fetch("http://localhost:3000/places")
       .then((response) => {
         return response.json();
       })
       .then((resData) => {
         setAvailablePlaces(resData.places);
+        setIsFetching(false)
       });
   }, []);
 
   // Alternatively, we can use async/await but it is not allowed inside the useEffect,
   // therefore we have to create a function and call it at the end, outside the function,
   // but inside the useEffect.
-  
+
   // useEffect(() => {
   //   const fetchPlaces = async () => {
   //     const response = await fetch("http://localhost:3000/places");
@@ -32,6 +35,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
